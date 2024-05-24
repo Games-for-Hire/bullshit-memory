28e286eb4152c94a387bff0daa83226259eb3a25
Philipp Haussleiter <philipp@haussleiter.de>
2024-05-24 16:59:20 +0200

    trigger deployment on push

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ const config = {
     sessionName: "LOCAL_DEV_SESSION",
     projectId: "65da39e5573f565f617e",
     endpoint: "https://db.myglobal.ws/v1",
-    redirect: "http://localhost:5502/?status=okay"
+    redirect: "http://localhost:5502/?status=okay",
+    version: "development"
 }
---
+++ prod/config.js
@@ -0,0 +1,9 @@
+const config = {
+    env: "prod",
+    context: "/bullshit-memory/",
+    sessionName: "PROD_SESSION",
+    projectId: "663ccc32dae944310b32",
+    endpoint: "https://db.myglobal.ws/v1",
+    redirect: "https://curiosdevcookie.github.io/bullshit-memory/?status=okay",
+    version: "--version--"
+}
