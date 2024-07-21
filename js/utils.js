cccd610670628b9ca8e9b2eae101f3f57b558e71
Philipp Haussleiter <philipp@haussleiter.de>
2024-07-21 19:55:05 +0200

    only use shared js library for auth stuff, use login and error handler

--- js/utils.js
+++ js/utils.js
@@ -11,16 +11,23 @@ const readCookie = function(key) {
     return value;
 }
 
+// see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie#samesitesamesite-value
 const setCookie = function (key, value) {
     var now = new Date();
     var expires = 1000 * 60 * 10;
     var expireDate = new Date(now.getTime() + expires);
-    document.cookie = key + "=" + value + "; expires=" + expireDate.toGMTString() + "; path=/;";
+    document.cookie = key + "=" + value + "; expires=" + expireDate.toGMTString() + "; path=/;SameSite=Strict;";
 }
 
 const clearCookie = function(key) {
     var now = new Date();
     var expires = 1000 * 60 * 10;
     var expireDate = new Date(now.getTime() - expires);
-    document.cookie = key + "=; expires=" + expireDate.toGMTString() + "; path=/;";
+    document.cookie = key + "=; expires=" + expireDate.toGMTString() + "; path=/;SameSite=Strict;";
+}
+
+const mapValues = function(objectMap, callback) {
+    for (const k in objectMap) {
+        callback(k, objectMap[k]);
+    }
 }
